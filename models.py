95326eacf614d340db3f61356afe1897b1175828
zepcp <vieirapereira@protonmail.com>
2019-12-06 22:11:43 +0000

    models2

--- models.py
+++ models.py
@@ -1,8 +1,14 @@
 from datetime import datetime
-from playhouse.postgres_ext import ArrayField
 
 import peewee
 import settings
+
+EMAIL_LENGTH = 128
+TRANSACTION_LENGTH = 66
+WALLET_LENGTH = 42
+DESCRIPTION_LENGTH = 320
+PROPOSAL_ID_LENGTH = 8
+NIF_LENGTH = 9
 
 db = peewee.PostgresqlDatabase(
     "gdg-devfest-19",
@@ -26,45 +32,45 @@ class NewsletterTelegram(BaseModel):
 
 
 class NewsletterEmail(BaseModel):
-    email = peewee.CharField(max_length=128, unique=True)
+    email = peewee.CharField(max_length=EMAIL_LENGTH, unique=True)
 
     class Meta:
         db_table = "newsletter_email"
 
 
 class Voter(BaseModel):
-    nif = peewee.CharField(max_length=320, unique=True)
-    wallet = peewee.CharField(max_length=16, unique=True)
+    nif = peewee.CharField(max_length=NIF_LENGTH, unique=True)
+    wallet = peewee.CharField(max_length=WALLET_LENGTH, unique=True)
     telegram_id = peewee.IntegerField(null=True)
-    email = peewee.DateTimeField(max_length=320, null=True)
+    email = peewee.CharField(max_length=EMAIL_LENGTH, null=True)
 
     class Meta:
         db_table = "voters"
 
 
 class Proposals(BaseModel):
-    id = peewee.FixedCharField(max_length=8, unique=True)
+    id = peewee.FixedCharField(max_length=PROPOSAL_ID_LENGTH, unique=True)
     deadline = peewee.IntegerField()
-    description = peewee.CharField(max_length=320, index=True)
+    description = peewee.CharField(max_length=DESCRIPTION_LENGTH, index=True)
 
     class Meta:
         db_table = "proposals"
 
 
 class Votes(BaseModel):
-    wallet = peewee.CharField(max_length=42, unique=True)
-    signature = peewee.CharField(max_length=66, unique=True)
+    wallet = peewee.CharField(max_length=WALLET_LENGTH, unique=True)
+    signature = peewee.CharField(max_length=TRANSACTION_LENGTH, unique=True)
     proposal_id = peewee.IntegerField()
     in_favor = peewee.BooleanField()
-    txid = peewee.CharField(max_length=66, null=True)
+    txid = peewee.CharField(max_length=TRANSACTION_LENGTH, null=True)
 
     class Meta:
         db_table = "votes"
 
 
 class Proofs(BaseModel):
-    proposal_id = peewee.FixedCharField(max_length=8, unique=True)
-    txid = peewee.FixedCharField(max_length=66, index=True)
+    proposal_id = peewee.FixedCharField(max_length=PROPOSAL_ID_LENGTH, unique=True)
+    txid = peewee.FixedCharField(max_length=TRANSACTION_LENGTH, index=True)
     deadline = peewee.IntegerField()
     updated = peewee.DateTimeField(default=datetime.utcnow, index=True)
 
